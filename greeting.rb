74454b010f8f586abc0863183c49fc3a67b062c0
Bethany Powell <beth@powellcastle.com>
2018-08-21 20:07:31 -0500

    Greeting file v2

--- greeting.rb
+++ greeting.rb
@@ -1,6 +1,8 @@
 def greeting
- # #1
-   ARGV.each do |arg|
-     puts "Yo #{arg}!"
+  greeting = ARGV.delete_at(0)
+   ARGV.each do |name|
+     puts "#{greeting} #{name}!"
    end
  end
+
+ greeting
