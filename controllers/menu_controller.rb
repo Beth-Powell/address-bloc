23c82db159cde9b4240e4ca459927562bd62adbf
Bethany Powell <beth@powellcastle.com>
2018-09-05 19:38:03 -0500

    Checkpoint5-assignment v4

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -7,47 +7,47 @@ class MenuController
     @address_book = AddressBook.new
   end
 
-   def main_menu
-     puts "Main Menu - #{address_book.entries.count} entries"
-     puts "1 - View all entries"
-     puts "2 - View Entry Number n"
-     puts "3 - Create an entry"
-     puts "4 - Search for an entry"
-     puts "5 - Import entries from a CSV"
-     puts "6 - Exit"
-     print "Enter your selection: "
+  def main_menu
+    puts "Main Menu - #{address_book.entries.count} entries"
+    puts "1 - View all entries"
+    puts "2 - View Entry Number n"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
+    print "Enter your selection: "
 
-     selection = gets.to_i
+    selection = gets.to_i
 
-     case selection
-       when 1
-         system "clear"
-         view_all_entries
-         main_menu
-       when 2
-         system "clear"
-         view_entry_number
-         main_menu
-       when 3
-         system "clear"
-         create_entry
-         main_menu
-       when 4
-         system "clear"
-         search_entries
-         main_menu
-       when 5
-         system "clear"
-         read_csv
-         main_menu
-       when 6
-         puts "Good-bye!"
-         exit(0)
-       else
-         system"clear"
-         puts "Sorry, that is not a valid input"
-         main_menu
-     end
+    case selection
+    when 1
+      system "clear"
+      view_all_entries
+      main_menu
+    when 2
+      system "clear"
+      view_entry_number
+      main_menu
+    when 3
+      system "clear"
+      create_entry
+      main_menu
+    when 4
+      system "clear"
+      search_entries
+      main_menu
+    when 5
+      system "clear"
+      read_csv
+      main_menu
+    when 6
+      puts "Good-bye!"
+      exit(0)
+    else
+      system"clear"
+      puts "Sorry, that is not a valid input"
+      main_menu
+    end
   end
 
   def view_all_entries
@@ -61,24 +61,28 @@ class MenuController
   end
 
   def view_entry_number
-    system "clear"
+    # system "clear"
     print "Entry Number to View: "
     entry_number = gets.chomp
+
+    Integer(entry_number)
+
     index = entry_number.to_i - 1
-      if entry_number.to_i < @address_book.entries.count
-        puts @address_book.entries[index]
-        puts "Press enter to return to the main menu"
-        gets.chomp
-        system "clear"
-        main_menu
-      if entry_number > @address_book.entries.count
-        puts "#{entry_number} is not a valid input"
-        view_entry_number
-      else
-        puts "#{entry_number} is not a valid input"
-        view_entry_number
-      end
+
+    if entry_number.to_i <= @address_book.entries.count
+      puts @address_book.entries[index]
+      puts "Press enter to return to the main menu"
+      gets.chomp
+      system "clear"
+      main_menu
+    else
+      puts "#{entry_number} is not a valid input"
+      view_entry_number
     end
+  rescue
+    puts "#{entry_number} is not a valid input"
+    view_entry_number
+  end
 
   def create_entry
     system "clear"
@@ -114,20 +118,19 @@ class MenuController
     selection = gets.chomp
 
     case selection
-      when "n"
+    when "n"
 
-      when "d"
+    when "d"
 
-      when "e"
+    when "e"
 
-      when "m"
-        system "clear"
-        main_menu
-      else
-        system "clear"
-        puts "#{selection} is not a valid input"
-        entry_submenu(entry)
+    when "m"
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      entry_submenu(entry)
     end
   end
-end
 end
